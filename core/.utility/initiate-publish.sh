8ca77a29e6341b1d6b35971ce3d3c98588b3ab7d
thecodequest <thecodequest@gmail.com>
2016-06-13 09:43:23 +0100

    Update initiate-publish.sh

--- core/.utility/initiate-publish.sh
+++ core/.utility/initiate-publish.sh
@@ -3,14 +3,14 @@
 # NOTE: Travis-CI can only publish SNAPSHOT versions. 
 
 if [ "$TRAVIS_REPO_SLUG" == "latency4j/repo" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
+  if [[ $(gradle -q getVersion) != *SNAPSHOT* ]]; then
       echo 'Travis can only publish snapshots.'
 #      return 0
   fi
 
   echo -e "Starting publish to Sonatype...\n"
 
-  ./gradlew uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+  gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then
