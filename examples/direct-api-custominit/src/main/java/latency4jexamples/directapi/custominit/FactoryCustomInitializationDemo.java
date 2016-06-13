b3ae6aac416f7cfd97d5ae2768de7fc0e25b0968
thecodequest <thecodequest@gmail.com>
2016-06-13 15:52:39 +0100

    Update FactoryCustomInitializationDemo.java

--- examples/direct-api-custominit/src/main/java/latency4jexamples/directapi/custominit/FactoryCustomInitializationDemo.java
+++ examples/direct-api-custominit/src/main/java/latency4jexamples/directapi/custominit/FactoryCustomInitializationDemo.java
@@ -1,39 +1,3 @@
-/*
-	Copyright (c) 2010 Obix Labs Limited
-	Redistribution and use in source and binary forms, 
-	with or without modification, are permitted provided 
-	that the following conditions are met:
-
-	*	Redistribution of source code must retain the above 
-		copyright notice, this list of conditions and the 
-		following disclaimer.
-	
-	*	Redistribution in binary form must reproduce the 
-		above copyright notice, this list of conditions 
-		and the following disclaimer in the documentation 
-		and/or other materials provided with the distribution.
-		
-	*	Neither the name of Obix Labs Limited nor the
-		names of its contributors may be used to endorse or 
-		promote products derived from this software without 
-		specific prior written permission.
-
-	
-	THIS SOFTWARE IS PROVIDED "AS IS," WITHOUT A WARRANTY OF 
-	ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS 
-	AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, 
-	FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, 
-	ARE HEREBY EXCLUDED. OBIX LABS LIMITED ("Obix Labs") AND ITS 
-	LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE 
-	AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR 
-	ITS DERIVATIVES. IN NO EVENT WILL Obix Labs OR ITS LICENSORS BE 
-	LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, 
-	INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE 
-	DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF 
-	LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS 
-	SOFTWARE, EVEN IF Obix Labs HAS BEEN ADVISED OF THE POSSIBILITY OF 
-	SUCH DAMAGES.
- */
 package latency4jexamples.directapi.custominit;
 
 import org.latency4j.LatencyMonitor;
